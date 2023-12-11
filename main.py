fd93badc1da7e998b1ba30aa6bfbb70a1b092196
Dursun Gokoglan <dursungokoglan.professional@gmail.com>
2023-12-12 00:14:55 +0300

    add branch main

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from tkinter import *
 from random import *
 import pandas as pd
 
-try:
-    database_file = pd.read_csv("data/words_to_learn.csv")
-except FileNotFoundError:
-    database_file = pd.read_csv("data/french_words.csv")
+lan1 = input("Enter source language code: ")
+lan2 = input("Enter translating language code: ")
+database_file = pd.read_csv(f"data/{lan1}{lan2}.csv")
 
 database = database_file.to_dict(orient="records")
 card = choice(database)
@@ -15,8 +14,8 @@ def skip_card():
     global card, timer
     card = choice(database)
     canvas.itemconfig(card_look, image=card_front_img)
-    canvas.itemconfig(language_info, text="French", fill="black")
-    canvas.itemconfig(word, text=card["French"], fill="black")
+    canvas.itemconfig(language_info, text=f"{lan1}", fill="black")
+    canvas.itemconfig(word, text=card[f"{lan1}"], fill="black")
     window.after_cancel(timer)
     timer = window.after(7000, flip_card)
 
@@ -26,16 +25,16 @@ def remove_card():
     database.remove(card)
     card = choice(database)
     canvas.itemconfig(card_look, image=card_front_img)
-    canvas.itemconfig(language_info, text="French", fill="black")
-    canvas.itemconfig(word, text=card["French"], fill="black")
+    canvas.itemconfig(language_info, text=f"{lan1}", fill="black")
+    canvas.itemconfig(word, text=card[f"{lan1}"], fill="black")
     window.after_cancel(timer)
     timer = window.after(7000, flip_card)
 
 
 def flip_card():
     canvas.itemconfig(card_look, image=card_back_img)
-    canvas.itemconfig(language_info, text="English", fill="white")
-    canvas.itemconfig(word, text=card["English"], fill="white")
+    canvas.itemconfig(language_info, text=f"{lan2}", fill="white")
+    canvas.itemconfig(word, text=card[f"{lan2}"], fill="white")
 
 
 def save():
@@ -53,8 +52,8 @@ canvas.grid(column=0, row=0, columnspan=2)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_look = canvas.create_image(400, 263, image=card_front_img)
-language_info = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text=card["French"], font=("Ariel", 60, "bold"))
+language_info = canvas.create_text(400, 150, text=f"{lan1}", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text=card[f"{lan1}"], font=("Ariel", 60, "bold"))
 
 wrong_img = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=skip_card)
@@ -66,5 +65,5 @@ right_button.grid(column=1, row=1)
 
 timer = window.after(7000, flip_card)
 
-window.protocol("WM_DELETE_WINDOW", save)
+# window.protocol("WM_DELETE_WINDOW", save)
 window.mainloop()
