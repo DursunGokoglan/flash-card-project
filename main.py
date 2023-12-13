d38ff65dcf208e288ffe5a9e413a984a02f02610
Dursun Gokoglan <dursungokoglan.professional@gmail.com>
2023-12-13 21:15:59 +0300

    fix bug

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from tkinter import *
 from random import *
 import pandas as pd
 
-lan1 = input("Enter source language code: ")
-lan2 = input("Enter translating language code: ")
-database_file = pd.read_csv(f"data/{lan1}{lan2}.csv")
+lan_list = ["eng", "fr", "de", "tr"]
+lan1 = input(f"Enter source language code {lan_list} : ")
+lan_list.remove(lan1)
+lan2 = input(f"Enter translating language code {lan_list} : ")
 
+database_file = pd.read_csv(f"data/{lan1}{lan2}.csv")
 database = database_file.to_dict(orient="records")
 card = choice(database)
 
@@ -52,8 +54,8 @@ canvas.grid(column=0, row=0, columnspan=2)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_look = canvas.create_image(400, 263, image=card_front_img)
-language_info = canvas.create_text(400, 150, text=f"{lan1}", font=("Ariel", 40, "italic"))
-word = canvas.create_text(400, 263, text=card[f"{lan1}"], font=("Ariel", 60, "bold"))
+language_info = canvas.create_text(400, 150, text=f"{lan1}", fill="black", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text=card[f"{lan1}"], fill="black", font=("Ariel", 60, "bold"))
 
 wrong_img = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=skip_card)
